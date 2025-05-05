645de20ab39edee373b6c2b2afe9515267ca9a29
PR0TEX <protex19@protonmail.com>
2025-05-05 20:58:06 +0200

    feat: remove docker install

--- install_k3s.sh
+++ install_k3s.sh
@@ -1,42 +1,16 @@
 #!/bin/bash
 
-# install_prerequisites() {
-#     apt-get update
-#     apt install curl nvidia-container-toolkit nvidia-container-runtime cuda-drivers-fabricmanager-535 nvidia-headless-535-server nvidia-utils-535-server 
-
-# }
-
-install_docker() {
-    # Add Docker's official GPG key:
-    apt-get update
-    apt-get install -y ca-certificates curl
-    install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    chmod a+r /etc/apt/keyrings/docker.asc
-
-    # Add the repository to Apt sources:
-    echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-    tee /etc/apt/sources.list.d/docker.list > /dev/null
-    apt-get update
-    apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-}
-
 install_nvidia_toolkit() {
-    apt-get update
     apt install -y curl
-    curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg &&
+    curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg --yes &&
         curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list |
-        sed 's#deb https://#deb &#91;signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' |
+        sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' |
             sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
 
     sed -i -e '/experimental/ s/^#//g' /etc/apt/sources.list.d/nvidia-container-toolkit.list
-    apt-get update
-    apt install -y nvidia-container-toolkit cuda-drivers-fabricmanager-535 nvidia-headless-535-server nvidia-utils-535-server
-    ubuntu-drivers install
-    # apt install nvidia-container-runtime 
+    apt update
+    apt install -y nvidia-container-toolkit nvidia-container-runtime cuda-drivers-fabricmanager-535 nvidia-headless-535-server nvidia-utils-535-server
 }
 
 install_kubectl() {
@@ -53,8 +27,6 @@ setup_gpu_on_k3s() {
     kubectl apply -f nvidia-device-plugin.yaml 
 }
 
-# install_prerequisites
-install_docker
 install_nvidia_toolkit
 
 if command -v kubectl &>/dev/null; then
